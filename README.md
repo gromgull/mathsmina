afce5b962155d446dbf9f75c3f17db5531a23c30
Gunnar Aastrand Grimnes <gromgull@gmail.com>
2017-09-22 22:38:49 +0200

    multiple games + readme

---
+++ README.md
@@ -0,0 +1,3 @@
+Sounds from SFX for games/apps pack by https://freesound.org/people/rhodesmas/
+
+https://freesound.org/people/rhodesmas/packs/17958/
--- game.js
+++ game.js
@@ -1,4 +1,11 @@
 
+function shuffle(a) {
+    for (let i = a.length; i; i--) {
+        let j = Math.floor(Math.random() * i);
+        [a[i - 1], a[j]] = [a[j], a[i - 1]];
+    }
+}
+
 class Stars extends PIXI.Container {
   constructor(x,y) {
     super();
@@ -65,6 +72,7 @@ class Board extends PIXI.Container {
 
   constructor() {
     super();
+    var board = this;
     var i;
     this.numbers = [];
 
@@ -75,38 +83,27 @@ class Board extends PIXI.Container {
       fill: 'white',
     };
 
+    var onTextDown = function () { board.onTextDown(this); };
+    var onTextUp = function () { board.onTextUp(this); };
+    var onTextOver = function () { board.onTextOver(this); };
+    var onTextOut = function () { board.onTextOut(this); };
+
     for ( i = 0 ; i<100 ; i++ ) {
       var t = new PIXI.Text(''+(1+i), style); //Object.assign({}, style));
+      t.num = i;
       this.addChild(t);
       this.numbers[i] = t;
-
+      t.hitArea = new PIXI.Rectangle(-35, -35, 70, 70);
       t.x = 15+70*(1+i%10);
       t.y = 15+70*Math.floor(1+i/10);
       t.anchor.set(0.5,0.5);
       t.interactive = true;
-      t.on('pointerdown', onTextDown)
+      t.on('pointerdown', onTextDown )
         .on('pointerup', onTextUp)
         .on('pointerupoutside', onTextUp)
         .on('pointerover', onTextOver)
         .on('pointerout', onTextOut);
     }
-
-    function onTextOut(t) {
-      this.style.fill='white';
-    }
-    function onTextOver(t) {
-      this.style.fill='red';
-    }
-    function onTextUp() {
-    }
-    function onTextDown() {
-      var s = new Stars(this.x, this.y);
-      game.main.addChild(s);
-      game.emitters.push(s);
-      s.emit();
-      if (game.sounds.success) game.sounds.success.play();
-    }
-
 
     var gfx = new PIXI.Graphics();
 
@@ -124,6 +121,94 @@ class Board extends PIXI.Container {
     }
 
     this.addChild(gfx);
+  }
+
+
+  onTextOut(t) {
+  }
+  onTextOver(t) {
+  }
+  onTextUp(t) {
+  }
+  onTextDown(t) {
+  }
+
+
+}
+
+class Board1 extends Board {
+  onTextOut(t) {
+    t.style.fill='white';
+  }
+  onTextOver(t) {
+    t.style.fill='red';
+  }
+  onTextUp(t) {
+  }
+  onTextDown(t) {
+    game.stars(t.x, t.y);
+    if (game.sounds.success) game.sounds.success.play();
+  }
+}
+
+
+class Board2 extends Board {
+  constructor() {
+    super();
+    this.numbers.forEach( n => n.style.fill = null );
+
+    this.targets = Array.from(Array(100).keys());
+    shuffle(this.targets);
+
+    for (var i=0 ; i<10 ; i++) {
+      var x = this.targets.pop();
+      this.numbers[x].style.fill = 'white';
+    }
+
+    var style = {
+      fontFamily: 'Encode Sans Expanded',
+      fontSize: 32,
+      fontWeight: 900,
+      fill: 'yellow',
+    };
+
+
+    this.text = new PIXI.Text('Wo ist ?', style);
+    this.text.x = 400;
+    this.text.y = 780;
+    this.text.anchor.set(0.5, 0.5);
+    this.addChild(this.text);
+
+    this.next();
+
+  }
+
+  next() {
+    this.target = this.targets.pop();
+    this.text.text = 'Wo ist '+(this.target+1)+'?';
+  }
+
+  onTextOut(t) {
+    //this.style.fill='white';
+  }
+  onTextOver(t) {
+    //this.style.fill='red';
+  }
+  onTextUp(t) {
+  }
+  onTextDown(t) {
+    if (this.target == t.num) {
+      t.style.fill='white';
+      game.stars(t.x, t.y);
+      if (game.sounds.success)
+        game.sounds.success.play();
+      this.target = this.targets.pop();
+      this.next();
+    } else {
+      if (game.sounds.fail)
+        game.sounds.fail.play();
+    }
+
   }
 }
 
@@ -152,7 +237,9 @@ class Game {
 
     document.body.appendChild(this.app.view);
 
-    // this will be 800x800
+    //this.app.view.requestFullscreen();
+
+    // this will always be 800x800
     this.main = new PIXI.Container();
 
     this.main.scale.set(Math.min(window.innerWidth, window.innerHeight)/800);
@@ -161,11 +248,18 @@ class Game {
     this.app.stage.addChild(this.main);
 
 
-    this.main.addChild(new Board());
+    this.main.addChild(new Board2());
 
     this.elapsed = Date.now();
     this.emitters = [];
 
+  }
+
+  stars(x,y) {
+    var s = new Stars(x, y);
+    this.main.addChild(s);
+    this.emitters.push(s);
+    s.emit();
   }
 
   update() {
